bd089e0176ca947fba78386577e00988f992fdf7
erresonj <erresonj@gmail.com>
2020-07-04 16:15:42 -0400

    initial commit for a blackjack game.

---
+++ cmake-build-debug/CMakeFiles/Comp322_Assignment2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Comp322_Assignment2.dir/blackjack.cpp.o"
+  "CMakeFiles/Comp322_Assignment2.dir/main.cpp.o"
+  "Comp322_Assignment2.exe"
+  "Comp322_Assignment2.pdb"
+  "libComp322_Assignment2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Comp322_Assignment2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
